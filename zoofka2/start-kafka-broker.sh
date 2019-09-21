aa16216b8e8882075e281c912760a7d02a938fad
Chen Alkabets <chen.alkabets@nova-lumos.com>
2019-09-21 14:05:24 +0300

    adding start script for single-broker (zoofka)

---
+++ zoofka2/start-kafka-broker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+docker build -t chenchuk/zoofka:1.0 .
+
+docker ps | grep 2181 && {
+  echo "container already running."
+  exit 99
+}
+
+
+# run container daemon
+export SERVER_IP=192.168.2.78
+docker run -d -p 2181:2181 \
+              -p 9092:9092 \
+              --env ADVERTISED_HOST=${SERVER_IP} \
+              --env ADVERTISED_PORT=9092 \
+              --name zoofka-$(date +%s) chenchuk/zoofka:1.0
